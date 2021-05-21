30bae99e253f9a681eec0531b40137992f661d5a
chiaminersf <83688360+chiaminersf@users.noreply.github.com>
2021-05-21 09:25:10 -0700

    Update manager.py
    test

--- src/plotman/manager.py
+++ src/plotman/manager.py
@@ -116,9 +116,9 @@ def maybe_start_new_plot(dir_cfg, sched_cfg, plotting_cfg):
             
             _, _, free_space = shutil.disk_usage(dstdir)
             free_space_in_GiB = free_space // (2**30)
-            if free_space_in_GiB < 102:
+            if free_space_in_GiB < 1802:
                 print("Not Enough space: current directory " + str(dstdir) + " has " + str(free_space_in_GiB) + " Gib")
-                while free_space_in_GiB < 102:
+                while free_space_in_GiB < 1802:
                     print("Try to find a dst with enough space")
                     if unused_dirs: 
                         dstdir = random.choice(unused_dirs)
